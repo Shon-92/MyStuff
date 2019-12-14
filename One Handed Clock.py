611e45a174ca467a4378d14973e6f5307760cd2c
Shon-92 <53130164+Shon-92@users.noreply.github.com>
2019-12-13 22:35:41 -0500

    Add files via upload

--- One Handed Clock.py
+++ One Handed Clock.py
@@ -4,4 +4,18 @@ current_time = time.localtime()
 
 hour = current_time.tm_hour
 
-print('The hour is', hour )
+minute = current_time.tm_min
+
+second = current_time.tm_sec
+
+year = current_time.tm_year
+
+month = current_time.tm_mon
+
+day = current_time.tm_mday
+
+print('The date is', month, day, year)
+
+print('The time is', hour, minute, second)
+
+
