6d9a210807382fc49509b35023f8dcf144bb903c
Shon-92 <53130164+Shon-92@users.noreply.github.com>
2019-12-13 22:05:11 -0500

    Add files via upload
    Pizza Pizza

---
+++ Pizza Pizza.py
@@ -0,0 +1,9 @@
+# Ordering Pizza Calculator
+
+students_text = input('How many students: ')
+
+students_int = int(students_text)
+
+pizza_count = int(students_int/1.5)+1
+
+print('You will need' , pizza_count, 'pizzas')
